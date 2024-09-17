2bcafce2bbaf24aabde3332d83c802eaaf23c5c0
Danny <142972494+DannyAkintunde@users.noreply.github.com>
2024-09-17 03:06:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import asyncio
 def setup_logging():
     logging.basicConfig(
         level=logging.DEBUG,
-        #format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         handlers=[
             logging.FileHandler('app.log'),
             logging.StreamHandler()
